7d7c8b4c14f9b033631f7ac56adb3fa9d20f90af
deadwishmen <deadwishmen@gmail.com>
2025-02-08 17:42:17 +0700

    update file dataset

--- dataset/visualizer_dataset.py
+++ dataset/visualizer_dataset.py
@@ -3,26 +3,32 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 from argparse import ArgumentParser
+import random
 
 
 def get_arg():
     parser = ArgumentParser()
-    parser.add_argument('--path_folder_npy', default='', type=str, help='Path to dataset folder images')
-    parser.add_argument('--path_csv', default='', type=str, help='Path to csv file')
+    parser.add_argument('--path_dataset', default='', type=str, help='Path to dataset folder images')
+    parser.add_argument('--save_test', default="", type=str, help='path save image visualizer')
+
 
     return parser.parse_args()
 
 
-def display_images(images_array, text_image, num_images=5):
+def display_images(images_array, text_image, num_images=5, save_path="output.png"):
+    random_indices = random.sample(range(len(images_array)), num_images)
     plt.figure(figsize=(10, 10))
-    for i in range(num_images):
+    for i, idx in enumerate(random_indices):
         plt.subplot(5, 5, i + 1)
         plt.xticks([])
         plt.yticks([])
         plt.grid(False)
-        plt.imshow(images_array[i], cmap=plt.cm.binary)
-        plt.xlabel(text_image[i])
+        plt.imshow(images_array[idx], cmap=plt.cm.binary)
+        print(text_image[idx])
+
+    plt.savefig(save_path, bbox_inches='tight', dpi=300)  # Lưu ảnh với chất lượng cao
     plt.show()
+
 
 
 
@@ -30,12 +36,11 @@ def display_images(images_array, text_image, num_images=5):
 def main(args):
     print(args.path_dataset)
     path_csv = os.path.join(args.path_dataset, 'train.csv')
-    path_images = os.path.join(args.path_dataset, 'train_images.npy')
-
+    path_images = os.path.join(args.path_dataset, 'train_context_bbox_arr.npy')
+    save_path = os.path.join(args.save_test, 'output.png')
     data_csv = pd.read_csv(path_csv)
     images = np.load(path_images)
-    print(data_csv.head())
-    display_images(images, data_csv['Output'].values)
+    display_images(images, data_csv['Output'].values, save_path = save_path)
     
 
 if __name__ == '__main__':
