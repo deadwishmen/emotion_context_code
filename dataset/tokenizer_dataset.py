e851d622218d2811b549d2c852c868e8300264fc
deadwishmen <deadwishmen@gmail.com>
2025-02-14 23:40:05 +0700

    update file dataset

--- dataset/tokenizer_dataset.py
+++ dataset/tokenizer_dataset.py
@@ -2,11 +2,11 @@ import pandas as pd
 from trasformers import DistilBertTokenizerFast
 
 
-def tokenizer_dataset(path_dataset):
+def tokenizer_text(path_dataset):
     tokenizer = DistilBertTokenizerFast.from_pretrained('bert-base-uncased')
     df = pd.read_csv(path_dataset)
 
-    sentences = df['text'].tolist()
+    sentences = df['Output'].tolist()
 
     tokens = tokenizer(sentences, padding=True, truncation=True, return_tensors='pt')
 
