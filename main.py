e04af4015adad808c3c5ae3a0c3b8b0d84559843
deadwishmen <deadwishmen@gmail.com>
2025-05-11 16:30:55 +0700

    first main

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from model.swin_transformer import swin_v2_t, swin_v2_s, swin_v2_b
 from model.vit import vit_b_16
 from model.fusion import FusionModel, FusionConcatModel, TransformerFusionModel, DualPathAttentionFusion, QFormer
 from dataset.data_loader import load_data, set_normalization_and_transforms
-from utils.losses import DiscreteLoss, CrossEtropyLoss, BCEWithLogitsLoss, FocalLoss
+from utils.losses import DiscreteLoss, BCEWithLogitsLoss, FocalLoss
 from utils.training import train_disc
 from utils.testing import test_disc
 from utils.predict import predict_and_show
@@ -182,8 +182,6 @@ def train(pars):
     disc_loss = DiscreteLoss('dynamic', device)
   elif loss_function == "BCE":
     disc_loss = BCEWithLogitsLoss('dynamic', device)
-  elif loss_function == "CrossEntropy":
-    disc_loss = CrossEtropyLoss('dynamic', device)
   elif loss_function == "FocalLoss":
     disc_loss = FocalLoss(gamma=2.0, alpha=None, weight_type='mean', device=device)
 
